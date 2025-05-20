d974ad3d9a57597997d35168483f0f346782f6fe
henry <devgony@gmail.com>
2025-05-20 16:01:34 +0900

    feat: implement type generator

---
+++ api/generator.ts
@@ -0,0 +1,50 @@
+import { generateApi } from "swagger-typescript-api";
+import path from "path";
+import fs from "fs";
+
+const generate = async (domain: string, url: string) => {
+  const PATH_TO_OUTPUT_DIR = path.resolve(
+    process.cwd(),
+    `./api/generated/${domain}`,
+  );
+
+  if (fs.existsSync(PATH_TO_OUTPUT_DIR)) {
+    console.log("🧹   Removing old generated files...");
+    fs.rmSync(PATH_TO_OUTPUT_DIR, { recursive: true, force: true });
+  }
+
+  generateApi({
+    output: PATH_TO_OUTPUT_DIR,
+    url,
+    httpClientType: "axios", // or "fetch"
+    defaultResponseAsSuccess: false,
+    generateClient: true,
+    generateRouteTypes: false,
+    generateResponses: true,
+    toJS: false,
+    extractRequestParams: false,
+    extractRequestBody: false,
+    extractResponseBody: false,
+    extractResponseError: false,
+    extractEnums: true,
+    unwrapResponseData: true,
+    defaultResponseType: "void",
+    singleHttpClient: true,
+    cleanOutput: false,
+    enumNamesAsValues: false,
+    moduleNameFirstTag: true,
+    moduleNameIndex: 0,
+    generateUnionEnums: false,
+    typePrefix: "",
+    typeSuffix: "",
+    enumKeyPrefix: "",
+    enumKeySuffix: "",
+    addReadonly: false,
+    sortTypes: false,
+    sortRoutes: false,
+    fixInvalidTypeNamePrefix: "Type",
+    fixInvalidEnumKeyPrefix: "Value",
+  });
+};
+
+generate("example", "https://petstore.swagger.io/v2/swagger.json");
