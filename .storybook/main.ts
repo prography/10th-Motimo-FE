b6cb35d9cc48201b3604c8d959b3ef697ffa97b1
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-05-22 14:38:04 +0900

    feat: 디자인 토큰 tailwind 적용 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,7 +13,7 @@ const config: StorybookConfig = {
         docs: false,
       },
     },
-    '@storybook/addon-onboarding',
+    '@storybook/addon-designs',
     '@storybook/addon-interactions',
   ],
   framework: {
