978b3aa61a69b7676b68a080879a9fdcac3e35df
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-05-24 14:35:52 +0900

    feat: token 변환 설정파일 수정
    구조 수정 및 tailwind prefix에 맞게 mapping

--- sd-tokens.config.mjs
+++ sd-tokens.config.mjs
@@ -1,20 +1,93 @@
 import StyleDictionary from "style-dictionary";
 
+const token2TailwindTypeMap = {
+  color: "color",
+  sizing: "dimension",
+  spacing: "dimension",
+  dimension: "dimension",
+  borderWidth: "border-witdh",
+  borderRadius: "radius",
+  border: "color",
+  opacity: "opacity",
+  boxShadow: "shadow",
+  fontFamilies: "font",
+  fontWeights: "font-weight",
+  lineHeights: "leading",
+  fintSizes: "font-size",
+  letterSpacing: "tracking",
+};
+
 // 커스텀 포맷 등록
 StyleDictionary.registerFormat({
   name: "css/theme-variables",
+
   format: function ({ dictionary }) {
-    console.log("dictionaory: ", dictionary);
     return [
       "@theme {",
-      ...dictionary.allTokens.map((prop) => `  --${prop.name}: ${prop.value};`),
+      ...dictionary.allTokens.map(
+        (prop) =>
+          `  --${token2TailwindTypeMap?.[prop.type] ?? "non-mapped"}-${prop.name}: ${prop.value};`,
+      ),
       "}",
     ].join("\n");
   },
 });
 
+const recursliveParsingHelper = (setName, curKey, curValue) => {
+  // 종료 조건
+  if (Object.keys(curValue).includes("type")) {
+    const prevValueHandled = curValue.value.slice(1, -1);
+    return { ...curValue, value: `{${setName}.${prevValueHandled}}` };
+  }
+
+  const resultValue = Object.entries(curValue).reduce(
+    (acc, [nextKey, innerObj]) => {
+      const resValue = recursliveParsingHelper(setName, nextKey, innerObj);
+      return { ...acc, [nextKey]: resValue };
+    },
+    {},
+  );
+  return { [curKey]: resultValue };
+};
+
+StyleDictionary.registerParser({
+  name: "json-parser",
+  pattern: /\.json$/,
+  parser: ({ filePath, contents }) => {
+    const parsed = JSON.parse(contents);
+    console.log("paresd", parsed["Semantic/light"].label.inverse);
+
+    const convertedParsed = Object.entries(parsed).reduce(
+      (accAllTokens, [setName, tokens]) => {
+        if (setName.startsWith("Primitive") || setName.startsWith("$"))
+          return { ...accAllTokens, [setName]: tokens };
+
+        const primitiveSet = setName.replace("Semantic", "Primitive");
+
+        const semanticTokens = Object.entries(tokens).reduce(
+          (accSemanticTokens, [key, value]) => {
+            const newSemanticTokens = recursliveParsingHelper(
+              primitiveSet,
+              key,
+              value,
+            );
+            return { ...accSemanticTokens, ...newSemanticTokens };
+          },
+          {},
+        );
+        return { ...accAllTokens, [setName]: semanticTokens };
+      },
+      {},
+    );
+
+    return convertedParsed;
+  },
+});
+
 export default {
   source: ["tokens/**/*.json"],
+
+  parsers: ["json-parser"],
   platforms: {
     css: {
       transformGroup: "css",
@@ -22,7 +95,6 @@ export default {
       files: [
         {
           format: "css/theme-variables",
-          // format: 'css/variables',
           destination: "tokens.css", //파일명
         },
       ],
