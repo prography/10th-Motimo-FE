2e05c0333483b29558356d7139347b09159fbe35
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-05-26 14:36:13 +0900

    feat: 디자인 시스템 스토리북 예시 수정

--- components/shared/Test.stories.tsx
+++ components/shared/Test.stories.tsx
@@ -1,5 +1,8 @@
+// Test.stories.tsx
 import Test from "./Test";
 import { StoryObj } from "@storybook/react";
+
+// meta는 공통 옵션.
 const meta = {
   title: "Example/Test",
   component: Test,
@@ -10,4 +13,26 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-export const Primary: Story = {};
+
+// Primary는 공통. argTypes를 사용해 상호작용 가능하도록 한다.
+export const Primary: Story = {
+  argTypes: {
+    children: {
+      control: "text",
+      description: "보통은 텍스트를 입력하겠지",
+      table: {
+        category: "이야호",
+      },
+    },
+  },
+  args: {
+    children: "버튼",
+  },
+};
+
+// 기타 스토리들은 args를 기본으로 간단히 처리함
+export const Ex1: Story = {
+  args: {
+    children: "기본값",
+  },
+};
