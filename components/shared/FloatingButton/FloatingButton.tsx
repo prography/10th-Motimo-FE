081e628410b087e7fa05db576581017c7fbbc32c
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-06-14 18:50:52 +0900

    feat: FloatingButton 컴포넌트 완성

---
+++ components/shared/FloatingButton/FloatingButton.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes } from "react";
+import PlusSvg from "../public/Add_Plus.svg";
+
+type FLoatingButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const FloatingButton = ({ ...props }: FLoatingButtonProps) => {
+  return (
+    <>
+      <button
+        {...props}
+        className="w-12 h-12 p-2 bg-indigo-500 rounded-[999px] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10)] inline-flex justify-center items-center gap-2"
+      >
+        <div className="w-6 h-6 relative overflow-hidden">
+          <PlusSvg width={24} height={24} />
+        </div>
+      </button>
+    </>
+  );
+};
+
+export default FloatingButton;
