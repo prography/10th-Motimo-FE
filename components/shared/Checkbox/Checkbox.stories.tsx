35ed3e7ce8ac587086bd1ff5f1005473c4bc9e91
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-06-15 16:11:14 +0900

    feat: Checkbox 컴포넌트 호버링 케이스 추가

--- components/shared/Checkbox/Checkbox.stories.tsx
+++ components/shared/Checkbox/Checkbox.stories.tsx
@@ -8,6 +8,11 @@ const meta = {
   parameters: {
     // Canvas 레이아웃을 중앙으로 정렬하거나 패딩을 추가할 수 있습니다.
     layout: "centered",
+    docs: {
+      description: {
+        component: "호버링 체크는 마우스를 직접 호버링 시켜야 합니다.",
+      },
+    },
   },
   // Docs 탭 자동 생성을 위해 필요합니다.
   tags: ["autodocs"],
@@ -29,11 +34,14 @@ export const Primary: Story = {
   args: {},
 };
 
+/** 마우스 호버링 시 outline 색이 변합니다. */
 export const UnChecked: Story = {
   args: {
     checked: false,
   },
 };
+
+/** 마우스 호버링 시 배경 색이 변합니다. */
 export const Checked: Story = {
   args: {
     checked: true,
