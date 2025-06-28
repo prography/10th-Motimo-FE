5b4cb54b2a6f0c16644a9ded6fdb5d2358a4350b
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-06-28 17:23:15 +0900

    feat: svgr 넥스트 설정 수정
    svgr을 icon화 하여 width와 height을 1em으로 변경하지 않도록 함.

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
           {
             loader: "@svgr/webpack",
             options: {
-              icon: true,
+              icon: false,
             },
           },
         ],
