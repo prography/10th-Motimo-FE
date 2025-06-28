8d3b7e63d8ffb3ac2becf13cd25e8ca672ff1f99
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-06-28 23:41:35 +0900

    feat: todo, todolist관련 swr 훅 제작

--- hooks/main/queries/useTodoList.ts
+++ hooks/main/queries/useTodoList.ts
@@ -1,11 +1,14 @@
-import { getTodosOnTodoList } from "@/api/main/todoListFetching";
-import useSWR from "swr";
+import { TodoRsStatusEnum } from "@/api/generated/motimo/Api";
+import { getTodosOnSubGoal } from "@/lib/main/subGoalFetching";
+import { TodoItemsInfo } from "@/types/todoList";
+import useSWR, { SWRConfiguration } from "swr";
 
 const mockFetcher = async () => {};
 
-const useTodoList = ({ todoListId }: { todoListId: string }) => {
-  const swr = useSWR(todoListId, getTodosOnTodoList);
-  return swr;
+const useTodoList = (subGoalId: string, options?: SWRConfiguration) => {
+  const { data, error, mutate } = useSWR(subGoalId, getTodosOnSubGoal, options);
+
+  return { data: data ?? [], error, mutate };
 };
 
 export default useTodoList;
