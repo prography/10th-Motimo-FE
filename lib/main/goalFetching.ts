ed391d96015584f12174b44dbcbf1d7b263f6420
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-06-29 06:40:45 +0900

    feat: goal 관련 fetching함수 제작

---
+++ lib/main/goalFetching.ts
@@ -0,0 +1,60 @@
+import { templateFetch } from "@/api/common/fetchTemplate";
+import {
+  GoalCreateRq,
+  GoalDetailRs,
+  GoalListRs,
+  GoalUpdateRq,
+} from "@/api/generated/motimo/Api";
+
+const updateGoal = async (goalId: string, newGoalInfo: GoalUpdateRq) => {
+  templateFetch;
+  // error처리 아직 안함. toast처리 해야 함
+  const result = await templateFetch(`/v1/goals/${goalId}`, "PUT", newGoalInfo);
+
+  // 타입 변환 해야할 수도
+  return result;
+};
+
+const getAllGoals = async () => {
+  const result = await templateFetch<GoalListRs>("/v1/goals", "GET");
+
+  return result;
+};
+
+const createNewGoal = async (newGoalInfo: GoalCreateRq) => {
+  const result = await templateFetch(`/v1/goals`, "POST", newGoalInfo);
+
+  return result;
+};
+
+const toggleGoalCompletion = async (goalId: string) => {
+  const result = await templateFetch(`/v1/goals/${goalId}/completion`, "PATCH");
+  return result;
+};
+
+const getGoal = async (goalId: string) => {
+  const result = await templateFetch<GoalDetailRs>(
+    `/v1/goals/${goalId}`,
+    "GET",
+  );
+
+  return result;
+};
+
+const getGoalWithSubGoalTodo = async (goalId: string) => {
+  const result = await templateFetch(
+    `/v1/goals/${goalId}/sub-goals/all`,
+    "GET",
+  );
+
+  return result;
+};
+
+export {
+  updateGoal,
+  getAllGoals,
+  createNewGoal,
+  toggleGoalCompletion,
+  getGoal,
+  getGoalWithSubGoalTodo,
+};
