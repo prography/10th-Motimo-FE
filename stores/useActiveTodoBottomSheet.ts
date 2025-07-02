16bb26ec0415d7ff8b63781faf24f3f9427a3a0b
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-07-02 15:44:44 +0900

    feat: TodoBottomSheet활성화, 선택된 goal관련 zustand store생성
    이들 데이터는 멀리 떨어진 컴포넌트 사이에서 사용되기 때문에 전역 상태 사용함.

---
+++ stores/useActiveTodoBottomSheet.ts
@@ -0,0 +1,20 @@
+import { TodoInfoForSubmission } from "@/components/shared/BottomSheets/TodoBottomSheet/TodoBottomSheet";
+import { create } from "zustand";
+
+interface ActiveTodoBottomSheetStore {
+  isActive: boolean;
+  initContent?: TodoInfoForSubmission;
+  setIsActive: (newState: boolean, initContent?: TodoInfoForSubmission) => void;
+}
+
+const useActiveTodoBottomSheet = create<ActiveTodoBottomSheetStore>((set) => ({
+  isActive: false,
+  initContent: undefined,
+  setIsActive: (newState, initContent) =>
+    set({
+      isActive: newState,
+      initContent: initContent,
+    }),
+}));
+
+export default useActiveTodoBottomSheet;
