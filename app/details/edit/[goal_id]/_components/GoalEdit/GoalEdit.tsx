93166851c0ee4a53ecfbd45844bb7fd283221986
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-07-08 22:49:00 +0900

    feat: 상세-편집 페이지GoalEdit 위치 변경 및 바텀시트와 모달 추가

---
+++ app/details/edit/[goal_id]/_components/GoalEdit/GoalEdit.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import EditInfo from "../../../../../../components/details/EditInfo/EditInfo";
+import TextField from "@/components/shared/TextField/TextField";
+import { Button } from "@/components/shared/Button/Button";
+import ModalDeletingGoal from "@/components/details/Modals/ModalDeletingGoal/ModalDeletingGoal";
+import GoalDurationBottomSheet from "@/components/details/BottomSheets/GoalDurationBottomSheet/GoalDurationBottomSheet";
+
+import TrashBin from "@/public/images/Trash_Full.svg";
+
+import { useState } from "react";
+import useModal from "@/hooks/useModal";
+
+interface GoalEditProps {
+  initGoalTitle: string;
+  initDurationValue?: unknown;
+  initDurationType?: unknown;
+}
+
+const GoalEdit = ({
+  initDurationValue,
+  initDurationType,
+  initGoalTitle,
+}: GoalEditProps) => {
+  const [goalTitle, setGoalTitle] = useState(initGoalTitle);
+  const { closeModal, openModal } = useModal();
+  const [openBottomSheet, setOpenBottomSheet] = useState(false);
+  return (
+    <>
+      <main>
+        <section>
+          <p>목표</p>
+          <TextField
+            name=""
+            isError={false}
+            value={goalTitle}
+            onChange={(e) => setGoalTitle(e.target.value)}
+          />
+        </section>
+        <section className="w-80 h-10 inline-flex justify-start items-center gap-4">
+          <div className="flex-1 flex justify-start items-center">
+            <p className="justify-start text-neutral-900 text-sm font-semibold font-['Pretendard'] leading-none">
+              기간 설정
+            </p>
+          </div>
+          <button
+            onClick={() => {
+              setOpenBottomSheet(true);
+            }}
+            type="button"
+            className="h-8 px-3 py-2 bg-background-assistive rounded flex justify-center items-center gap-2"
+          >
+            <p className="justify-start text-neutral-900 text-sm font-normal font-['Pretendard'] leading-none">
+              {`${initDurationType} - ${initDurationValue}`}
+            </p>
+          </button>
+        </section>
+        <div>
+          <Button
+            icon={
+              <div className="text-status-negative w-5 h-5 inline-flex justify-center items-center">
+                <TrashBin />
+              </div>
+            }
+            size="s"
+            variant="text"
+            onClick={() => {
+              openModal(
+                <ModalDeletingGoal
+                  onClose={closeModal}
+                  onDeleteGoal={async () => {
+                    console.log("삭제");
+                  }}
+                />,
+              );
+            }}
+          >
+            <p className="text-center justify-start text-status-negative text-sm font-semibold font-['Pretendard'] leading-tight">
+              목표 삭제
+            </p>
+          </Button>
+        </div>
+      </main>
+      <GoalDurationBottomSheet
+        openBottomSheet={openBottomSheet}
+        setopenBottomSheet={setOpenBottomSheet}
+        onEdit={async ({ type, value }) => {
+          console.log("duration type, value: ", type, value);
+          // 닫기
+          setOpenBottomSheet(false);
+        }}
+      />
+    </>
+  );
+};
+export default GoalEdit;
