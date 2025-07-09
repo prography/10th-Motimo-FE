c89f939c3d23d6334d5b464f970469d3097a5cb8
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-07-09 18:09:04 +0900

    feat: 상세페이지 라우팅 적용

--- app/details/[goal_id]/page.tsx
+++ app/details/[goal_id]/page.tsx
@@ -1,20 +1,25 @@
+"use client";
+
 import { AppBar } from "@/components/shared";
 import { Button } from "@/components/shared/Button/Button";
 import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Details() {
+  const { goal_id: goalId } = useParams<{ goal_id: string }>();
+  const router = useRouter();
   return (
     <>
       <div>
         <header>
           <AppBar
             onBackClick={() => {
-              console.log("뒤로가기 라우팅");
+              router.back();
             }}
             type="back"
             title="목표 상세페이지"
           />
-          <Link href={""}>
+          <Link href={`/details/edit/${goalId}`}>
             <button className="w-12 h-8 p-2 bg-background-normal rounded inline-flex justify-center items-center gap-2">
               <p className="flex-1 text-center justify-center text-label-alternative text-xs font-semibold font-['SUIT_Variable'] leading-none">
                 편집
