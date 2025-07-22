f5698c2ab1132ceaf8972398b643d8ada1e1d9ff
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-07-22 12:33:44 +0900

    fix: DetailBody에서 목표 완료 이후 목표 완료시키는 버튼 보이지 않도록 수정

--- app/details/[goal_id]/_components/DetailsBody/DetailBody.tsx
+++ app/details/[goal_id]/_components/DetailsBody/DetailBody.tsx
@@ -82,7 +82,7 @@ const DetailBody = ({ goalId }: DetailBodyProps) => {
           isCompleted={goalDetail?.isCompleted ?? false}
         />
         <section className="flex flex-col gap-4 pl-4 pr-4 pb-4 bg-background-alternative">
-          {allSubGoalCompleted && (
+          {allSubGoalCompleted && !goalDetail?.isCompleted && (
             <>
               <button
                 type="button"
