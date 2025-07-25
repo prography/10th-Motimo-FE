5f2c1c8bcc0857e864913408ff088566e3d12d8c
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-07-25 21:07:17 +0900

    feat: 상세 페이지에서의 DetailBody 뒤로가기 버튼 처리 수정
    상세 페이지로부터의 이동은 그냥 뒤로가기로,
    나머지는 group페이지로 이동하도록 수정
    group페이지로부터 채팅방 이동 시 무한 로딩 생겨서.

--- app/group/[id]/page.tsx
+++ app/group/[id]/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { useParams, useRouter } from "next/navigation";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import GroupChatRoom from "@/components/group/groupRoom/GroupChatRoom/GroupChatRoom";
 import GroupRoomHeader from "@/components/group/groupRoom/GroupRoomHeader/GroupRoomHeader";
 import { useGroupDetail } from "@/api/hooks";
+
 export default function GroupRoom() {
   const { id: groupId } = useParams<{ id: string }>();
   const { data } = useGroupDetail(groupId);
   const router = useRouter();
+  const fromPath = useSearchParams().get("from");
+
   return (
     <main className="flex flex-col gap-4 bg-background-alternative min-h-screen pb-4 relative">
       <GroupRoomHeader
         groupRoomName={data?.name ?? ""}
-        onBackClick={() => router.push(`/group`)}
+        onBackClick={() => {
+          if (fromPath && fromPath === "details") {
+            router.back();
+          }
+          router.push("/group");
+        }}
         routeToMember={() => router.push(`/group/${groupId}/member`)}
       />
       <GroupChatRoom groupId={groupId} />
