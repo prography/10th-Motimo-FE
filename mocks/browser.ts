80b7d35d2065ddb457cc2ba0e74035eb207882b8
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-08-17 17:33:29 +0900

    feat: 게스트 모드 위한 모킹 구조 제작

--- mocks/browser.ts
+++ mocks/browser.ts
@@ -1,4 +1,5 @@
 import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
+// import { handlers } from "./handlers";
+import guestHandlers from "./guestMode/handlers";
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...guestHandlers);
