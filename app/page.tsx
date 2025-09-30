5bbfca34c087cf40ad425f1469cd25012372fd8c
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-09-30 18:38:58 +0900

    feat: hydration 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,117 @@
-"use client";
+// "use client";
 
-import dynamic from "next/dynamic";
-import GoalMenuContainer from "@/components/main/GoalMenuContainer/GoalMenuContainer";
-import GoalCard from "@/components/main/GoalCard/GoalCard";
-import MainHeader from "@/components/main/MainHeader/MainHeader";
-import { BottomTabBar } from "@/components/shared/BottomTabBar/BottomTabBar";
-import { useMyProfile } from "@/api/hooks";
-import { calcLeftDay } from "@/utils/calcLeftDay";
-import GoalDataContainer from "@/components/main/GoalDataContainer/GoalDataContainer";
+import api from "@/api/service";
+import Main, { FallbackProvider } from "./Main";
+import { queryArgs } from "@/api/queries";
+import ServerAuthGuard from "./_components/ServerAuthGuard";
+
+// import dynamic from "next/dynamic";
+// import GoalMenuContainer from "@/components/main/GoalMenuContainer/GoalMenuContainer";
+// import GoalCard from "@/components/main/GoalCard/GoalCard";
+// import MainHeader from "@/components/main/MainHeader/MainHeader";
+// import { BottomTabBar } from "@/components/shared/BottomTabBar/BottomTabBar";
+// import { useMyProfile } from "@/api/hooks";
+// import { calcLeftDay } from "@/utils/calcLeftDay";
+// import GoalDataContainer from "@/components/main/GoalDataContainer/GoalDataContainer";
 
 // AuthGuard는 클라이언트에서만 렌더링 (localStorage 접근 필요)
-const AuthGuard = dynamic(() => import("./_components/AuthGuard"), {
-  ssr: false,
-});
+// const AuthGuard = dynamic(() => import("./_components/AuthGuard"), {
+//   ssr: false,
+// });
 
-export default function Main() {
-  const { data } = useMyProfile();
-  const tmpDaysOfServiceUse = data?.createdAt 
-    ? calcLeftDay(new Date(), new Date(data.createdAt))
-    : 0;
+// export default function Main() {
+//   const { data } = useMyProfile();
+//   const tmpDaysOfServiceUse = data?.createdAt
+//     ? calcLeftDay(new Date(), new Date(data.createdAt))
+//     : 0;
 
+//   return (
+//     <AuthGuard>
+//       <section className="w-full h-full ">
+//         <div
+//           data-icon="false"
+//           data-type="main"
+//           className="w-full h-full min-h-screen pb-14 relative bg-white inline-flex flex-col flex-1 justify-start  gap-1"
+//         >
+//           <MainHeader daysOfServiceUse={tmpDaysOfServiceUse} />
+//           <GoalMenuContainer />
+//           {/* <GoalCard /> */}
+//           <GoalDataContainer />
+//         </div>
+//       </section>
+//       <BottomTabBar className="fixed z-40 bottom-0" type="1" />
+//     </AuthGuard>
+//   );
+// }
+
+export default async function MainPage() {
+  // const initUserData = await api.사용자Api.getMyProfile();
+  // const initCheerData = await api.응원Api.getCheerPhrase();
+  // const initPointData = await api.포인트Api.getPoint();
+  // const initGoalsData = await api.목표Api.getGoalList();
+
+  // const profileKey = JSON.stringify(queryArgs.myProfile);
+  // const cheerKey = JSON.stringify(queryArgs.cheerPhrase);
+  // const pointKey = JSON.stringify(queryArgs.points);
+  // const goalsKey = JSON.stringify(queryArgs.goals);
+
+  // //test
+  // console.log(
+  //   "initdatas: ",
+  //   initUserData,
+  //   initCheerData,
+  //   initPointData,
+  //   initGoalsData,
+  // );
+
+  // const { data: cheerData } = useCheerPhrase();
+  //   const { data: pointData } = usePoints();
+  //   const { data: rawGoalData, mutate } = useGoals();
   return (
-    <AuthGuard>
-      <section className="w-full h-full ">
-        <div
-          data-icon="false"
-          data-type="main"
-          className="w-full h-full min-h-screen pb-14 relative bg-white inline-flex flex-col flex-1 justify-start  gap-1"
-        >
-          <MainHeader daysOfServiceUse={tmpDaysOfServiceUse} />
-          <GoalMenuContainer />
-          {/* <GoalCard /> */}
-          <GoalDataContainer />
-        </div>
-      </section>
-      <BottomTabBar className="fixed z-40 bottom-0" type="1" />
-    </AuthGuard>
+    <>
+      {/* <FallbackProvider
+        fallback={{
+          [profileKey]: initUserData,
+          [cheerKey]: initCheerData,
+          [pointKey]: initPointData,
+          [goalsKey]: initGoalsData,
+        }}
+      >
+        <Main />
+      </FallbackProvider> */}
+      <ServerAuthGuard>
+        <MainHydration />
+      </ServerAuthGuard>
+    </>
   );
 }
+
+const MainHydration = async () => {
+  const initUserData = await api.사용자Api.getMyProfile();
+  const initCheerData = await api.응원Api.getCheerPhrase();
+  const initPointData = await api.포인트Api.getPoint();
+  const initGoalsData = await api.목표Api.getGoalList();
+
+  const profileKey = JSON.stringify(queryArgs.myProfile);
+  const cheerKey = JSON.stringify(queryArgs.cheerPhrase);
+  const pointKey = JSON.stringify(queryArgs.points);
+  const goalsKey = JSON.stringify(queryArgs.goals);
+
+  // const { data: cheerData } = useCheerPhrase();
+  //   const { data: pointData } = usePoints();
+  //   const { data: rawGoalData, mutate } = useGoals();
+  return (
+    <>
+      <FallbackProvider
+        fallback={{
+          [profileKey]: initUserData,
+          [cheerKey]: initCheerData,
+          [pointKey]: initPointData,
+          [goalsKey]: initGoalsData,
+        }}
+      >
+        <Main />
+      </FallbackProvider>
+    </>
+  );
+};
