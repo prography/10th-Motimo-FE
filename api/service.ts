62c65197d1d08c65d1fe4bcd6f76bf11fc15310a
SangHyeon Lee <67571350+Hys-Lee@users.noreply.github.com>
2025-10-30 18:07:18 +0900

    fix: 토큰 재발급 서버에서 리프레시 토큰 사용하도록 수정
    cookie에 리프레시 토큰도 저장하도록 수정 및 관련 서버 함수 제작

--- api/service.ts
+++ api/service.ts
@@ -3,6 +3,7 @@ import useAuthStore from "../stores/useAuthStore";
 import useToastStore from "@/stores/useToastStore";
 import { cookies } from "next/headers";
 import { getToken } from "./getToken";
+import { getRefreshToken } from "./getRefreshToken";
 
 // HTTP 클라이언트 생성 시 인증 헤더를 자동으로 추가하는 securityWorker 설정
 const httpClient = new HttpClient({
@@ -100,14 +101,14 @@ const tokenHandler = async <T, E>(
 ) => {
   return apiRes.catch(async (e) => {
     if (e.status === 401) {
-      let token;
+      let refreshToken;
       if (typeof window === "undefined") {
-        token = await getToken();
+        refreshToken = await getRefreshToken();
       } else {
-        token = useAuthStore.getState().refreshToken;
+        refreshToken = useAuthStore.getState().refreshToken;
       }
 
-      if (!token) {
+      if (!refreshToken) {
         // api.authController.logout();
         // window.location.href = "/";
 
@@ -123,7 +124,7 @@ const tokenHandler = async <T, E>(
       // 웹용 처리
       try {
         const tokenRes = await api.authController.reissue({
-          refreshToken: token || undefined,
+          refreshToken: refreshToken || undefined,
         });
 
         if (!tokenRes?.accessToken || !tokenRes?.refreshToken) {
